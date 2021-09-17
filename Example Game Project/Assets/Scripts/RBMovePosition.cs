d92df0317fe99d5f41ab16a205817f8ab5d9a8ad
Katrin69 <kawaii9393@mail.ru>
2021-09-17 12:32:48 +0300

    Add RB Move Position

--- Example Game Project/Assets/Scripts/RBMovePosition.cs
+++ Example Game Project/Assets/Scripts/RBMovePosition.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RGAddForce : MonoBehaviour
+public class RBMovePosition : MonoBehaviour
 {
     public float forceMult = 200;
     private Rigidbody rb;
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
     }
     void Update()
     {
-        rb.AddForce(transform.forward * forceMult * Time.deltaTime); 
+        Vector3 newPosition = (transform.position + (transform.forward * Time.deltaTime));
+        rb.MovePosition(newPosition);
     }
 }
