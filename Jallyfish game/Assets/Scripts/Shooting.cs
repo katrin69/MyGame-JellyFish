5c51104b7152e9a2c5165a4a91b47ba7231fc196
Katrin69 <kawaii9393@mail.ru>
2021-09-22 20:33:48 +0300

    Add Shooting

---
+++ Jallyfish game/Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Shoot();
+        }
+
+
+        //  if (Input.GetMouseButton(1))
+        //  {
+        //      GameObject newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        //     newBullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletForce;
+        //  }
+
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode.Impulse);
+    }
+}
