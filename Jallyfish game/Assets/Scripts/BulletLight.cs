f3a73b6bc08cfcbbb5e19efa83984ee3eca54b8e
Katrin69 <kawaii9393@mail.ru>
2021-09-26 15:01:19 +0300

    Add Timer

--- Jallyfish game/Assets/Scripts/BulletLight.cs
+++ Jallyfish game/Assets/Scripts/BulletLight.cs
@@ -6,6 +6,7 @@ public class BulletLight : MonoBehaviour
 {
     private float bulletForce = 24f;
     [SerializeField] private Rigidbody rb;
+    public float Timer = 5;
 
     private void FixedUpdate()
     {
@@ -15,6 +16,10 @@ public class BulletLight : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Shark"))
+        {
+            gameObject.SetActive(false);
+        }
+        else if (Timer < 0)
         {
             gameObject.SetActive(false);
         }
