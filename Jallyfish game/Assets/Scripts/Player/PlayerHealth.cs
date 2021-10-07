d2ee1f005ebcb7003b9fffdbea2a8e9a856bf6ba
Katrin69 <kawaii9393@mail.ru>
2021-10-07 11:57:50 +0300

    Add game over 2

--- Jallyfish game/Assets/Scripts/Player/PlayerHealth.cs
+++ Jallyfish game/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 
 public class PlayerHealth : MonoBehaviour
 {
     int curHp; //текущее количество жизней
     int maxHp = 10; //максимальное количество жизней
+    public float reastartDelay = 4f;//задержка перезапуска
 
     private void Start()
     {
@@ -22,9 +25,13 @@ public class PlayerHealth : MonoBehaviour
         PlayerHealthBar.instance.SetValue(curHp / (float)maxHp);
         if (curHp <= 0)
         {
-
+            Invoke(nameof(Restart), reastartDelay); //задержка перезапуска
             print("—ћ≈–“№");
         }
     }
 
+    void Restart() //метод загружает сцены с перезапруском
+    { 
+        SceneManager.LoadScene(1);
+    }
 }
