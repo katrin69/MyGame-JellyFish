ff0d2f14d27d7eca33fa16fed496ba32d2865a37
Katrin69 <kawaii9393@mail.ru>
2021-10-23 18:16:00 +0300

    Add Bullet Homing 2

--- Jallyfish game/Assets/Scripts/Weapon/BulletJellyHoming.cs
+++ Jallyfish game/Assets/Scripts/Weapon/BulletJellyHoming.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 
 public class BulletJellyHoming : MonoBehaviour
 {
-    Transform target; //Наша цель 
-    public float bulletSpeed = 10f;
-    private Rigidbody rb;
     [SerializeField] float damageEnemy = 3f; //Величина урона
+    public float bulletSpeed = 10f;
 
+    // Transform target; //Наша цель 
+    private Rigidbody rb;
+    private Vector3 target;
+    Transform enemy;
 
     private LevelsSystem ShooterLevelSystem; //брём систему уровней
 
 
     private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Shark").transform; //Цель равно обьекту с тэгом Акула 
-        rb = GetComponent<Rigidbody>();
+        //target = GameObject.FindGameObjectWithTag("Shark").transform; //Цель равно обьекту с тэгом Акула 
+        //rb = GetComponent<Rigidbody>();
+
+        enemy = GameObject.FindGameObjectWithTag("Shark").transform;
+        target = new Vector3(enemy.position.x, enemy.position.y, enemy.position.z);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 targetdirection = target.position - transform.position;
-        transform.LookAt(target);
-        rb.velocity = targetdirection.normalized * bulletSpeed;
+        transform.position = Vector3.MoveTowards(transform.position, enemy.position , bulletSpeed * Time.deltaTime); //перемещаемся к таргеты с опр скорость
     }
+
+    //private void FixedUpdate()
+    // {
+    //Vector3 targetdirection = target.position - transform.position;
+    //transform.LookAt(target);
+    //rb.velocity = targetdirection.normalized * bulletSpeed;
+
+
+    // }
 
     public void SetShooterLevelsSystem(LevelsSystem shooterLevelSystem) //система левлов на сохранение
     {
