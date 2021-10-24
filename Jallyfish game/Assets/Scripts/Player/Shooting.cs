78a61d3ee86db09776c8799024d0aa9929d082ab
Katrin69 <kawaii9393@mail.ru>
2021-10-24 14:59:55 +0300

    Add Bullet Fart 2

--- Jallyfish game/Assets/Scripts/Player/Shooting.cs
+++ Jallyfish game/Assets/Scripts/Player/Shooting.cs
@@ -7,14 +7,32 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab; //молния
     public Transform firePoint; //место от куда стрелять
     public float bulletForce = 20f; //скорость движения молнии
-    //Никита любит Лёшу
+                                    //Никита любит Лёшу
 
+    LevelsSystem levelSystem;
+
+    //для большого пука
+    [SerializeField] ParticleSystem _bulletFart;
+    [SerializeField] float damageEnemyFart = 4f; //Величина урона
+
+
+    private void Start()
+    {
+       levelSystem = GetComponent<LevelsSystem>(); // достаём наши левлы ссылочный тип
+
+    }
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) //если нажата кнопка то стрелять
         {
             Shoot();
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ShootFart();
+        }
+
 
     }
 
@@ -24,27 +42,44 @@ public class Shooting : MonoBehaviour
 
         if (bullet != null) //если пуля не пуста то 
         {
-            LevelsSystem levelSystem = GetComponent<LevelsSystem>(); // достаём наши левлы ссылочный тип
 
             if (levelSystem != null) //если наш левл не пуст то 
             {
                 BulletLight bulletLight = bullet.GetComponent<BulletLight>(); //достаём пулю
                 bulletLight.SetShooterLevelsSystem(levelSystem);  //отдаёт систему левла на сохранение когда выпускает пулю
             }
-            else
-            {
-                BulletJelly bulletJelly = bullet.GetComponent<BulletJelly>(); //достаём пулю
-                bulletJelly.SetShooterLevelsSystem(levelSystem);  //отдаёт систему левла на сохранение когда выпускает пулю
-            }
+            //сделать другой метод
             //if (levelSystem != null)
             //{
-            //    BulletJellyHoming bulletJellyHoming = bullet.GetComponent<BulletJellyHoming>(); //достаём пулю
-            //    bulletJellyHoming.SetShooterLevelsSystem(levelSystem);
+            //    BulletJelly bulletJelly = bullet.GetComponent<BulletJelly>(); //достаём пулю
+            //    bulletJelly.SetShooterLevelsSystem(levelSystem);  //отдаёт систему левла на сохранение когда выпускает пулю
             //}
 
             bullet.transform.position = firePoint.position;
             bullet.transform.rotation = transform.rotation;
             bullet.SetActive(true);
+        }
+    }
+
+    public void ShootFart()
+    {
+        _bulletFart.Play();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 6f);
+        foreach (Collider c in colliders)
+        {
+            if (c.gameObject.CompareTag("Shark"))
+            {
+                if (levelSystem != null)
+                {
+
+                    ////Получаем скрипт здоровья акулы
+                    EnemyHealth enemyHealthScript = c.transform.GetComponent<EnemyHealth>();
+                    //передаём урон
+                    enemyHealthScript.DeductHealth(damageEnemyFart, levelSystem);
+
+                }
+            }
         }
     }
 }
