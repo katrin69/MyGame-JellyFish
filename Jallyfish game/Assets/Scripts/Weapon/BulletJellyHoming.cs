cc6225bfdc598bc02f3efb75e06c53ad1c4d08d2
Katrin69 <kawaii9393@mail.ru>
2021-10-24 20:32:30 +0300

    Add little sad

--- Jallyfish game/Assets/Scripts/Weapon/BulletJellyHoming.cs
+++ Jallyfish game/Assets/Scripts/Weapon/BulletJellyHoming.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 
 public class BulletJellyHoming : MonoBehaviour
 {
-    //[SerializeField] float damageEnemy = 3f; //Величина урона
+    [SerializeField] float damageEnemy = 3f; //Величина урона
     public float bulletSpeed = 10f;
-    public GameObject bulletHoming;
-    public GameObject target;
-    //public List<GameObject> spawn;
+    Transform target; //Наша цель 
+    private Rigidbody rb;
 
-
-    //// Transform target; //Наша цель 
-    //private Rigidbody rb;
     //private Vector3 target;
     //Transform enemy;
 
@@ -23,8 +19,8 @@ public class BulletJellyHoming : MonoBehaviour
     private void Start()
     {
         //1
-        //target = GameObject.FindGameObjectWithTag("Shark").transform; //Цель равно обьекту с тэгом Акула 
-        //rb = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Shark").transform; //Цель равно обьекту с тэгом Акула 
+        rb = GetComponent<Rigidbody>();
 
         //2
         //enemy = GameObject.FindGameObjectWithTag("Shark").transform;
@@ -38,29 +34,16 @@ public class BulletJellyHoming : MonoBehaviour
         //2
         //transform.position = Vector3.MoveTowards(transform.position, enemy.position , bulletSpeed * Time.deltaTime); //перемещаемся к таргеты с опр скорость
 
-        //3
-        bulletHoming.transform.LookAt(target.transform);
-        StartCoroutine(SendHoming(bulletHoming));
+        
     }
 
 
     //1
-    //private void FixedUpdate()
-    // {
-    //Vector3 targetdirection = target.position - transform.position;
-    //transform.LookAt(target);
-    //rb.velocity = targetdirection.normalized * bulletSpeed;
-   // }
-
-    public IEnumerator SendHoming(GameObject bulletHoming)
+    private void FixedUpdate()
     {
-        while (Vector3.Distance(target.transform.position, bulletHoming.transform.position) > 0.3f)
-        {
-            bulletHoming.transform.position += (target.transform.position - bulletHoming.transform.position).normalized * bulletSpeed * Time.deltaTime;
-            bulletHoming.transform.LookAt(target.transform);
-            yield return null;
-        }
-        Destroy(bulletHoming);
+        Vector3 targetdirection = target.position - transform.position;
+        transform.LookAt(target);
+        rb.velocity = targetdirection.normalized * bulletSpeed;
     }
 
     public void SetShooterLevelsSystem(LevelsSystem shooterLevelSystem) //система левлов на сохранение
