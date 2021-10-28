7a75e6b7f5c92a874e33143a8a233e3a6c578fe9
Katrin69 <kawaii9393@mail.ru>
2021-10-28 14:57:07 +0300

    Add test 2

--- Jallyfish game/Assets/Scripts/Player/Kuraga.cs
+++ Jallyfish game/Assets/Scripts/Player/Kuraga.cs
@@ -6,4 +6,6 @@ public class Kuraga : MonoBehaviour
 {
     // Start is called before the first frame update
     public float kuraga = 5f; //i love
+    public float kuraga2 = 5f; //Курага любит you
+
 }
