87fa169dc72db4029c09f55253a95e8cc20cc93e
Katrin69 <kawaii9393@mail.ru>
2021-10-28 14:40:20 +0300

    add

---
+++ Jallyfish game/Assets/Scripts/Player/Test.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Test : MonoBehaviour
+{
+   public float kuraga = 5f; //курага какает
+    //kakaet
+}
