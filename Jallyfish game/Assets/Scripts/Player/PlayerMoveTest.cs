60554581f9411e4452c2821fab2b24b5cd8ef264
Katrin69 <kawaii9393@mail.ru>
2021-10-28 11:38:09 +0300

    test

--- Jallyfish game/Assets/Scripts/Player/PlayerMoveTest.cs
+++ Jallyfish game/Assets/Scripts/Player/PlayerMoveTest.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#pragma warning disable CS1633 // Unrecognized #pragma directive
+#pragma execution_character_set("utf-8")
 
 public class PlayerMoveTest : MonoBehaviour
+#pragma warning restore CS1633 // Unrecognized #pragma directive
 {
+
     public float kuraga = 5f; //это курага сан и он ссыт
 
     private void Start()
