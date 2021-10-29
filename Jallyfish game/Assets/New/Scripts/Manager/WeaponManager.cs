45b583860284f8586578735dfce11fe391fbe5ca
Katrin69 <kawaii9393@mail.ru>
2021-10-29 17:23:03 +0300

    Add weapon

--- Jallyfish game/Assets/New/Scripts/Manager/WeaponManager.cs
+++ Jallyfish game/Assets/New/Scripts/Manager/WeaponManager.cs
@@ -1,8 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    //шарит за оружие
+    private WeaponBulletLight WeaponBulletLight; //скрипты с оружием
+    private WeaponBulletJelly WeaponBulletJelly;
+    private WeaponJellyHoming WeaponJellyHoming;
+    private WeaponFart WeaponFart;
    
 }
---
+++ Jallyfish game/Assets/New/Scripts/Weapon/WeaponFart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFart : MonoBehaviour
+{
+    //большой пук урон 4
+
+    [SerializeField] ParticleSystem _bulletFart;
+    private float damageEnemyFart = 4f; //урон
+
+    public void ShootFart()
+    {
+        _bulletFart.Play();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 6f);
+        foreach (Collider c in colliders)
+        {
+            if (c.gameObject.CompareTag("Shark"))
+            {
+               //урон врагу
+            }
+        }
+    }
+}
